3eed78c2dcbad89f41aec25c6eb04424538b5197
kidintwo3 <kidintwo3@gmail.com>
2016-11-20 17:23:36 +0100

    version up

--- src/PluginMain.cpp
+++ src/PluginMain.cpp
@@ -18,7 +18,7 @@
 MStatus initializePlugin( MObject obj )
 {
 	MStatus		status;
-	MFnPlugin	fnPlugin( obj, "Creative Case", "1.6", "Any");
+	MFnPlugin	fnPlugin( obj, "Creative Case", "1.61", "Any");
 
 	icons_data_write();
 
